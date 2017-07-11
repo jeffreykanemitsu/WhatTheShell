bb4b5136190a6fdd194c1a45e20729922d8d1072
jeffreykanemitsu <jeffreykanemitsu@outlook.com>
2017-07-11 05:42:53 +0000

    task fork+wait+execve: completed

--- fwe.c
+++ fwe.c
@@ -1,1 +1,29 @@
 #include "shell.h"
+
+int main(void)
+{
+	char *argv[] = {"/bin/ls", "-l", "/tmp/", NULL};
+	pid_t pid;
+	int i, NUM_TIMES, status;
+
+	printf("Parent PID: %d\n", getpid());
+
+	for (i = 0; i <= 4; i++)
+	{
+		pid = fork();
+		if (pid < 0)
+		{
+			return (-1);
+		}
+		if (pid == 0)
+		{
+			printf("Parent PID: %d, Child ID: %d\n", i, getpid());
+			execve(argv[0], argv, NULL);
+		}
+		else
+		{
+			wait(&status);
+		
+		}
+	}
+}
