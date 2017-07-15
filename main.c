d905417ada7aa5cf1963b902f55a195f7e3508de
jeffreykanemitsu <jeffreykanemitsu@gmail.com>
2017-07-15 00:35:12 +0000

    changed ggez.c to main.c

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
 #include "shell.h"
-int main(void)
+int main()
 {
 	char *buffer, *token, *argv[1024];
 	int value, status;
 	size_t buffer_size;
-	pid_t pid;
+	pid_t pid, childpid;
 	unsigned int i;
 
-	write(STDOUT_FILENO, "$ ", 1);
+	write(1, "$ ", 2);
 	
 	value = getline(&buffer, &buffer_size, stdin);
-	if (value != -1)
+	while (value != -1)
 	{
-		token = strtok(buffer, " ");
+		token = strtok(buffer, " \n");
 
 		for (i = 0; token != NULL; i++)
 		{
 			argv[i] = token;
-			token = strtok(NULL, " ");
+			token = strtok(NULL, " \n");
 		}
 		argv[i] = NULL;
 
@@ -25,16 +25,21 @@ int main(void)
 
 		if (pid < 0)
 		{
+			perror("Error");
 			return (1);
 		}
 		if (pid == 0)
 		{
 			execve(argv[0], argv, NULL);
+			perror("Error execve");
+			return (1);
 		}
 		else
 		{
-			wait(&status);	
+			wait(&status);
+			write(1, "$ ", 2);
+			getline(&buffer, &buffer_size, stdin);
 		}
-		return(1);
 	}
+	return (0);
 }
